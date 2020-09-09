25abd52f40be07298f515b3cd73e912f37e2c5b4
Erik Michelson <github@erik.michelson.eu>
2020-09-09 18:55:37 +0200

    Add toggle functionality to button

--- src/components/editor/app-bar/dark-mode-button.tsx
+++ src/components/editor/app-bar/dark-mode-button.tsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react'
+import React, { useCallback } from 'react'
 import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
 import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { ApplicationState } from '../../../redux'
+import { setDarkMode } from '../../../redux/dark-mode/methods'
 import { ForkAwesomeIcon } from '../../common/fork-awesome/fork-awesome-icon'
 
 const DarkModeButton: React.FC = () => {
   const { t } = useTranslation()
-  const [buttonState, setButtonState] = useState(false)
-  const buttonToggle = () => {
-    setButtonState(prevState => !prevState)
-  }
+  const darkModeEnabled = useSelector((state: ApplicationState) => state.darkMode.darkMode)
+
+  const toggleDarkMode = useCallback(() => {
+    setDarkMode(!darkModeEnabled)
+  }, [darkModeEnabled])
 
   return (
-    <ToggleButtonGroup type="checkbox" defaultValue={[]} name="dark-mode" className="ml-2" value={buttonState ? ['dark'] : ['']}>
+    <ToggleButtonGroup type="checkbox" defaultValue={[]} name="dark-mode" className="ml-2" value={darkModeEnabled ? ['dark'] : ['']}>
       <ToggleButton
-        title={ buttonState ? t('editor.darkMode.switchToLight') : t('editor.darkMode.switchToDark')}
-        variant={ buttonState ? 'secondary' : 'light' }
-        className={ buttonState ? 'text-white' : 'text-secondary' }
-        onChange={buttonToggle} value={'dark'}
+        title={ darkModeEnabled ? t('editor.darkMode.switchToLight') : t('editor.darkMode.switchToDark')}
+        variant={ darkModeEnabled ? 'secondary' : 'light' }
+        className={ darkModeEnabled ? 'text-white' : 'text-secondary' }
+        onChange={toggleDarkMode} value={'dark'}
       >
-        {buttonState
+        {darkModeEnabled
           ? <ForkAwesomeIcon icon="sun"/>
           : <ForkAwesomeIcon icon="moon"/>
         }
