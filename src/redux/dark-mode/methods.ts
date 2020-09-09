5f31a32df84f308c75e5cc48b783f73a9de3d88f
Erik Michelson <github@erik.michelson.eu>
2020-09-09 12:58:23 +0200

    Add automatic detection and localStorage saving

--- src/redux/dark-mode/methods.ts
+++ src/redux/dark-mode/methods.ts
@@ -1,5 +1,5 @@
 import { store } from '..'
-import { DarkModeConfigActionType, SetDarkModeConfigAction } from './types'
+import { DarkModeConfig, DarkModeConfigActionType, SetDarkModeConfigAction } from './types'
 
 export const setDarkMode = (darkMode: boolean): void => {
   const action: SetDarkModeConfigAction = {
@@ -7,4 +7,47 @@ export const setDarkMode = (darkMode: boolean): void => {
     darkMode: darkMode
   }
   store.dispatch(action)
+}
+
+export const saveToLocalStorage = (darkModeConfig: DarkModeConfig): void => {
+  try {
+    window.localStorage.setItem('nightMode', String(darkModeConfig.darkMode))
+  } catch (e) {
+    console.error('Saving dark-mode setting to local storage failed: ', e)
+  }
+}
+
+export const loadFromLocalStorage = (): DarkModeConfig | undefined => {
+  try {
+    const storedValue = window.localStorage.getItem('nightMode')
+    if (!storedValue) {
+      return undefined
+    }
+    return {
+      darkMode: storedValue === 'true'
+    }
+  } catch (e) {
+    console.error('Loading dark-mode setting from local storage failed: ', e)
+    return undefined
+  }
+}
+
+export const determineDarkModeBrowserSetting = (): DarkModeConfig | undefined => {
+  try {
+    const mediaQueryResult = window.matchMedia('(prefers-color-scheme: dark)').matches
+    return {
+      darkMode: mediaQueryResult
+    }
+  } catch (e) {
+    console.error('Can not determine dark-mode setting from browser: ', e)
+    return undefined
+  }
+}
+
+export const updateBodyClass = (darkModeConfig: DarkModeConfig): void => {
+  if (darkModeConfig.darkMode) {
+    window.document.body.classList.add('dark')
+  } else {
+    window.document.body.classList.remove('dark')
+  }
 }
--- src/redux/dark-mode/reducers.ts
+++ src/redux/dark-mode/reducers.ts
@@ -1,17 +1,27 @@
 import { Reducer } from 'redux'
+import { determineDarkModeBrowserSetting, loadFromLocalStorage, saveToLocalStorage, updateBodyClass } from './methods'
 import { DarkModeConfig, DarkModeConfigActions, DarkModeConfigActionType, SetDarkModeConfigAction } from './types'
 
-export const initialState: DarkModeConfig = {
-  darkMode: false
+export const getInitialState = (): DarkModeConfig => {
+  const initialMode = loadFromLocalStorage() ?? determineDarkModeBrowserSetting() ?? {
+    darkMode: false
+  }
+  updateBodyClass(initialMode)
+  saveToLocalStorage(initialMode)
+  return initialMode
 }
 
-export const DarkModeConfigReducer: Reducer<DarkModeConfig, DarkModeConfigActions> = (state: DarkModeConfig = initialState, action: DarkModeConfigActions) => {
+export const DarkModeConfigReducer: Reducer<DarkModeConfig, DarkModeConfigActions> = (state: DarkModeConfig = getInitialState(), action: DarkModeConfigActions) => {
+  let darkModeConfigState: DarkModeConfig
   switch (action.type) {
     case DarkModeConfigActionType.SET_DARK_MODE:
-      return {
+      darkModeConfigState = {
         ...state,
         darkMode: (action as SetDarkModeConfigAction).darkMode
       }
+      updateBodyClass(darkModeConfigState)
+      saveToLocalStorage(darkModeConfigState)
+      return darkModeConfigState
     default:
       return state
   }
